2c1d3d06b92823ef6573d7d63967d3cfa2e61ffa
Opti-Dev <drestep@aggies.ncat.edu>
2026-06-15 20:30:34 -0400

    build and lint fixes

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h> // <-- CRUCIAL in PlatformIO!
+
+void setup() {
+    // Put your setup code here, to run once:
+    Serial.begin(115200);
+}
+
+void loop() {
+    // Put your main code here, to run repeatedly:
+}
